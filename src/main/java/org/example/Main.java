0807e793697342ce352145940306b17f3e079ea6
Abdullah <abdkhan033@gmail.com>
2024-09-13 08:44:47 -0400

    Updating Notes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,25 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        Student student = new Student("Abdullah", "Computer Science", 19, "R01301941");
+        // parameterized constructor
+        Student student = new Student("Abdullah", "Computer Science", 19, "R012345678");
         System.out.println(student.getName() + "\n" + student.getMajor() + "\n" + student.getAge() + "\n" + student.getRamId());
 
         System.out.println();
 
-        Student student2 = new Student("Billy", "Nursing", 23, "R12345678");
+        // second parameterized constructor
+        Student student2 = new Student("Billy", "Nursing", 23, "R012345687");
         System.out.println(student2.getName() + "\n" + student2.getMajor() + "\n" + student2.getAge() + "\n" + student2.getRamId());
 
         System.out.println();
 
+        // default constructor
         Student student3 = new Student();
         System.out.println(student3.getName() + "\n" + student3.getMajor() + "\n" + student3.getAge() + "\n" + student3.getRamId());
 
         System.out.println();
 
+        // default constructor + setting values
         Student student4 = new Student();
         student4.setName("Johnny");
         student4.setMajor("Physics");
